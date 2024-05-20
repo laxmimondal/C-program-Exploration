0c92b869ca28bdd265e29b9911ccefc5d0ad1878
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-05-21 02:48:51 +0530

    Update pp4_02.c

--- pp4_02.c
+++ pp4_02.c
@@ -1,10 +1,11 @@
 // multipication table in reverse order
 #include<stdio.h>
 int main(){
-    int n ;
+    int n,i ;
  printf("Enter n\n");
     scanf("%d",&n);
-    for(int i=10; i>0; i--)
+    printf("the multiplication order of %d in reverse is\n",n);
+    for(i=10; i>0; i--)
     {
       printf("%d X %d = %d\n ",n,i, n*i);
     }
