6f22bef7b9769d66b925550873d3f1c2a0f4abb7
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-05-21 02:45:19 +0530

    Update pp4_01.c

--- pp4_01.c
+++ pp4_01.c
@@ -1,7 +1,9 @@
 // multipication table
 #include<stdio.h>
 int main(){
-    int n = 3;
+    int n ;
+ printf("Enter n\n");
+    scanf("%d",&n);
     for(int i = 1; i < 11; i++)
     {
       printf("%d X %d = %d\n ",n,i, n*i);
