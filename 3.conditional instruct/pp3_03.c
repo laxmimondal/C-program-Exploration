24e7d86fc15fd9146a83e27b9128f11a9b52929e
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-05-20 21:25:48 +0530

    Update pp3_03.c

--- 3.conditional instruct/pp3_03.c
+++ 3.conditional instruct/pp3_03.c
@@ -8,18 +8,18 @@ int main()
     scanf("%d",&marks2);
     printf("Enter marks3\n");
     scanf("%d",&marks3);
-    float total = (marks1 + marks2 + marks3)/4;
+    float total = (marks1 + marks2 + marks3)/3;
     if (total>=40)
     {
         if(marks1<33||marks2<33||marks3<33){
         printf("You failed less marks in sub");
         }
         else{
-            printf("you passed");
+            printf("Your percentage is %d &you passed",total);
         }
     }
     else{
-        printf("you failed");
+        printf("Your percentage is %d & you failed",total);
     }
 
 
