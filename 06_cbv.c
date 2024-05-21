7d1a2bfcf46af7c66487ee1a4cf4777d7654ed0d
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-05-21 16:51:07 +0530

    Update 06_cbv.c

--- 06_cbv.c
+++ 06_cbv.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-int sum(int , int);
+int sum(int , int); //declaration of function 
 int main()
 {
     int a = 5;
     int b = 4;
-
-    printf("the value of a+b is %d\n", sum(a, b));
-    printf("the value of a and b is %d and %d\n", a, b);
-
+    printf("The value of a and b is %d and %d\n", a, b);
+    printf("The value of a+b is %d\n", sum(a, b));
     return 0;
 }
 int sum(int a, int b)
