d0f2d7779a5a747718cd6ab969b03ba09bb50a8f
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-05-21 18:15:47 +0530

    Update 06_cbr.c

--- 06_cbr.c
+++ 06_cbr.c
@@ -5,13 +5,14 @@ int main()
 {
     int a = 5;
     int b = 4;
-    printf("the value of a+b is %d\n", sum(&a, &b));
-    printf("the value of a and b is %d and %d\n", a, b);
-
+printf("The value of a and b is %d and %d\n", *(&a), b);
+printf("The value of a+b  is %d\n", sum(&a, &b));
     return 0;
 }
 int sum(int *a, int *b)
 {
     *a = 56;
+     printf("There is change in value of a\n");
     return *a + *b;
+   
 }
